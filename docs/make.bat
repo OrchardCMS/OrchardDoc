c9d345a53cdb435d8afa07a0d8cc7305ddad968e
Matthew Harris <rtpharry@hotmail.com>
2016-04-23 20:48:12 +0100

    update make to support the livehtml command

--- docs/make.bat
+++ docs/make.bat
@@ -21,6 +21,7 @@ if "%1" == "help" (
 	echo.  html       to make standalone HTML files
 	echo.  dirhtml    to make HTML files named index.html in directories
 	echo.  singlehtml to make a single large HTML file
+	echo.  livehtml   to rebuild the HTML files when a change is detected
 	echo.  pickle     to make pickle files
 	echo.  json       to make JSON files
 	echo.  htmlhelp   to make HTML files and a HTML help project
@@ -84,6 +85,12 @@ if "%1" == "singlehtml" (
 	goto end
 )
 
+if "%1" == "livehtml" (
+	sphinx-autobuild -b html %ALLSPHINXOPTS% %BUILDDIR%/html
+	if errorlevel 1 exit /b 1
+	goto end
+)
+
 if "%1" == "pickle" (
 	%SPHINXBUILD% -b pickle %ALLSPHINXOPTS% %BUILDDIR%/pickle
 	if errorlevel 1 exit /b 1
@@ -115,9 +122,9 @@ if "%1" == "qthelp" (
 	echo.
 	echo.Build finished; now you can run "qcollectiongenerator" with the ^
 .qhcp project file in %BUILDDIR%/qthelp, like this:
-	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\complexity.qhcp
+	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\aspnet.qhcp
 	echo.To view the help file:
-	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\complexity.ghc
+	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\aspnet.ghc
 	goto end
 )
 
