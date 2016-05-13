d057a0a88055e162e6c65788fd8d4da54b786da4
Antoine Griffard <agriffard@hotmail.com>
2016-05-13 18:06:29 +0200

    Contributors

---
+++ rtd/js/app.js
@@ -0,0 +1,7 @@
+var app = angular.module('ContributorsApp', ['ContributorsApp.Controllers']);
+
+app.config(['$httpProvider', function($httpProvider) {
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    }
+]);
